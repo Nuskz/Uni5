365b395c3c262187347517cbc558b4f0b9bc41b0
Ari Elias da Silva Júnior <ari@furb.br>
2022-05-09 19:15:51 -0300

    f

--- src/Uni5Exe01.java
+++ src/Uni5Exe01.java
@@ -4,11 +4,16 @@ public class Uni5Exe01 {
     public static void main(String[] args) {
         // Scanner
         Scanner teclado = new Scanner(System.in);
-        int numero = teclado.nextInt();
+
+        // Fim do Scanner
+
+        for (int contador = 1; contador < 20; contador++) {
+
+            System.out.println("Informe" + contador + "° número: ");
+            int numero = teclado.nextInt();
+            System.out.println("O número é " + (numero % 2 == 0 ? "par" : "impar"));
+        }
 
         teclado.close();
-        // Fim do Scanner
-        for (numero = 1; numero < 20; numero++);
-        System.out.println("Informe");
     }
 }
