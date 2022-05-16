3325c074771256bf61629d029e504e5e7aaf0778
Ari Elias da Silva Júnior <ari@furb.br>
2022-05-16 19:36:02 -0300

    5 incom

---
+++ src/Uni5Exe05.java
@@ -0,0 +1,12 @@
+public class Uni5Exe05 {
+    public static void main(String[] args) {
+        
+        int termos = 0;
+        for (int i = 2; i < 100; i += 2) {
+            termos = (i + 2);
+            
+            System.out.println(termos);
+
+        }
+    }
+}
