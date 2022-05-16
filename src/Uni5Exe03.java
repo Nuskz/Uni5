222ce9cb0cd2f825b69748c4c9ad669f29aeff85
Ari Elias da Silva Júnior <ari@furb.br>
2022-05-16 19:15:34 -0300

    3com

---
+++ src/Uni5Exe03.java
@@ -0,0 +1,14 @@
+
+public class Uni5Exe03 {
+    public static void main(String[] args) {
+
+        double total = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            total += (1d / i);
+
+        }
+        System.out.println("Total: " + total);
+
+    }
+}
