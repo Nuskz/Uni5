b7ad82fe5375b72e151190f97acf36454ba3f897
Nuskz <arielliasjr200@gmail.com>
2022-05-17 14:56:49 -0300

    ...

--- src/Uni5Exe11.java
+++ src/Uni5Exe11.java
@@ -1,22 +1,22 @@
 /*Uma máquina de biscoito está com problemas. Quando ligada, após 1 hora ela quebra 1 biscoito, na segunda hora ela quebra 3 biscoitos, 
 na hora seguinte ela quebra 3 vezes a quantidade de biscoitos quebrados na hora anterior, e assim por diante. 
 Faça um algoritmo que calcule quantos biscoitos são quebrados no final de cada dia (a máquina opera 16 horas por dia).
-
+COMPLETAAAA!!!!
 */
 public class Uni5Exe11 {
     public static void main(String[] args) {
+        
+        
         int biscoitoQuebrado = 1;
         
-        System.out.println("1h: " + biscoitoQuebrado + " biscoito quebrado");
-        
-        for (int i = 2; i <= 16; i++ ) {
-            if (i == 2) {
-                biscoitoQuebrado += 2;
+        for (int i = 1; i <= 16; i++ ) {
+            if (i == 16) {
+                biscoitoQuebrado ++;
 
-            } else if (i > 2) {
+            } else {
                 biscoitoQuebrado = biscoitoQuebrado * 3;
             }
-            System.out.println(i + "h: " + biscoitoQuebrado + " biscoitos quebrados");
         }
+        System.out.println("Foram: " + biscoitoQuebrado + " biscoitos quebrados no final do dia ");
     }
 }
--- src/Uni5Exe12.java
+++ src/Uni5Exe12.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
+/*Escreva um programa que leia um número inteiro positivo n e em seguida imprima n linhas do chamado Triangulo de Floyd:
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+16 17 18 19 20 21
+Triangulo de Floyd
+*/
 public class Uni5Exe12 {
     public static void main(String[] args) {
         
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Quantas linhas você deseja que o triângulo tenha? ");
+        int linhaTri = teclado.nextInt();
+
+        int num = 1;
+        String quebraTri = " ";
+        for (int i = 1; i <= linhaTri; i++) {
+             for (int t = 1; t <= i; t++) {
+                System.out.print(num + quebraTri);
+                num += 1;
+             }
+             System.out.println();
+        }
+        teclado.close();
     }
 }
