6935958dc31e8dfc25005e8fffacb40f1428bd7a
Nuskz <arielliasjr200@gmail.com>
2022-05-21 13:34:53 -0300

    ..<.<<

--- src/Uni5Exe33.java
+++ src/Uni5Exe33.java
@@ -1,5 +1,86 @@
-public class Uni5Exe33ter {
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * Em uma eleição para presidência, existem 4 candidatos.
+ * Os votos são informados através de código.
+ * Os dados utilizados para votação obedecem a seguinte codificação:
+ * 
+ * 1, 2, 3, 4 = voto para o respectivo candidato;
+ * 5 = voto nulo;
+ * 6 = voto em branco.
+ * Elabore um programa que calcule e escreva:
+ * 
+ * total de votos para cada candidato;
+ * total de votos nulos;
+ * total de votos em branco;
+ * percentual dos votos em branco e nulos sobre o total.
+ * Se o usuário informar um número de operação incorreto,
+ * emitir a mensagem “Opção incorreta” e persistir solicitando um número de
+ * opção correto.
+ * Para interromper a operação, o usuário poderá fornecer o número 0.
+ * CONCLUIDA!!!!
+ */
+public class Uni5Exe33 {
     public static void main(String[] args) {
-     
- }   
+        Scanner input = new Scanner(System.in);
+        DecimalFormat deci = new DecimalFormat("0.00");
+
+        int cand1 = 0, cand2 = 0, cand3 = 0, cand4 = 0;
+        int nulo = 0;
+        int branco = 0;
+        int voto = 0;
+
+        do {
+            System.out.println(
+                    "Informe o seu voto:\n1, 2, 3 ou 4 para os candidatos\n5 para nulo \n6 para votar em branco");
+            voto = input.nextInt();
+
+            switch (voto) {
+                case 1:
+                    cand1++;
+                    break;
+
+                case 2:
+                    cand2++;
+                    break;
+
+                case 3:
+                    cand3++;
+                    break;
+
+                case 4:
+                    cand4++;
+                    break;
+
+                case 5:
+                    nulo++;
+                    break;
+
+                case 6:
+                    branco++;
+                    break;
+
+                default:
+                    System.out.println("Opção incorreta");
+
+            }
+
+        } while (voto != 0);
+
+        double totalVotos = cand1 + cand2 + cand3 + cand4 + nulo + branco;
+        double perNulo = (nulo / totalVotos) * 100;
+        double perBranco = (branco / totalVotos) * 100;
+
+        System.out.println("Candidato 1: " + cand1 + "\nCandidato 2: " + cand2 
+        + "\nCandidato 3: " + cand3 + "\nCandidato 4: " + cand4
+        + "\n Votos em branco: " + branco + "\nVotos nulos: " + nulo);
+
+        System.out.println("Percentual de votos:\nBrancos: " + deci.format(perBranco) + "% " 
+        + "Nulos: " + deci.format(perNulo) + "% ");
+
+        input.close();
+    }
 }
+
+
--- src/Uni5Exe34ter.java
+++ src/Uni5Exe34ter.java
@@ -1,5 +1,80 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/*
+ Um hotel cobra R$ 50,00 de diária por hóspede e mais uma taxa de serviços. A taxa de serviços é de:
+
+R$ 7,50 por diária, caso o número de diárias seja menor que 15;
+R$ 6,50 por diária, caso o número de diárias seja igual a 15;
+R$ 5,00 por diária, caso o número de diárias seja maior que 15.
+Descreva um algoritmo que apresente as seguintes opções ao recepcionista:
+
+(1) encerrar a conta de um hóspede;
+(2) verificar número de contas encerradas;
+(3) sair.
+Caso a opção escolhida seja a primeira, leia o nome e o número de diárias do hóspede e escreva o nome e total a ser pago. 
+Caso a opção escolhida seja a segunda, informe o número de hóspedes que deixaram o hotel (número de contas encerradas). 
+Caso a opção escolhida seja a terceira finalize a execução do algoritmo.
+ */
 public class Uni5Exe34ter {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        DecimalFormat deci = new DecimalFormat("0.00");
+
+        String nome = "";
+        int diarias = 0;
+        int opcao = 0;
+        int contas = 0;
+
+        double totalPagar = 0;
+        double valorDiarias = 0;
+        do {
+            System.out.println(
+                    "(1) Encerrar a conta de um hóspede\n(2) Verificar número de contas encerradas\n(3) Sair.");
+            opcao = input.nextInt();
+
+            switch (opcao) {
+
+                case 1:
+                    contas++;
+                    System.out.print("Informe o nome do(a) hóspede: ");
+                    nome = input.next();
+
+                    System.out.print("\nInforme quantos dias ele passou: ");
+                    diarias = input.nextInt();
+
+                    if (diarias < 15) {
+                        valorDiarias = 7.5;
+                        totalPagar = valorDiarias * diarias;
+
+                        System.out.println("Hóspede: " + nome + "\nValor a pagar: R$" + deci.format(totalPagar));
+
+                    } else if (diarias == 15) {
+
+                        valorDiarias = 6.5;
+                        totalPagar = valorDiarias * diarias;
+                        System.out.println("Hóspede: " + nome + "\nValor a pagar: R$" + deci.format(totalPagar));
+
+                    } else {
+                        valorDiarias = 5;
+                        totalPagar = valorDiarias * diarias;
+                        System.out.println("Hóspede: " + nome + "\nValor a pagar: R$" + deci.format(totalPagar));
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Contas encerradas: " + contas);
+                    break;
+
+                case 3:
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+        } while (opcao != 3);
+
+        input.close();
     }
 }
